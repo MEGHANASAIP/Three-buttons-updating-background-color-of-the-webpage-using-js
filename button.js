0affa587966de5cf49f4446a0e05efc787103148
MEGHANA SAI P <gmsc2074@gmail.com>
2024-10-05 06:26:05 +0530

    Add files via upload

---
+++ button.js
@@ -0,0 +1,16 @@
+function redcolor()
+{
+    document.body.style.backgroundColor="red";
+}
+function bluecolor()
+{
+    document.body.style.backgroundColor="blue";
+}
+function greencolor()
+{
+    document.body.style.backgroundColor="green";
+}
+function resetcolor()
+{
+    document.body.style.backgroundColor="";
+}
